5cb4774ba3cc418953f36b46eabb871c35b6e6fe
Jacob Bijani <jacob.bijani@gmail.com>
2017-12-01 17:02:37 -0500

    structure response, need to get ajax in

--- voice-memos/index.js
+++ voice-memos/index.js
@@ -8,11 +8,11 @@ const app = express();
 app.use(urlencoded({ extended: false }));
 
 app.post('/voice', (request, response) => {
-    // Use the Twilio Node.js SDK to build an XML response
     const twiml = new VoiceResponse();
 
+    // gather calling_code from user
     const gather = twiml.gather({
-        numDigits: 5,
+        numDigits: 6,
         action: '/gather',
     });
     gather.say('Enter the calling code');
@@ -20,21 +20,38 @@ app.post('/voice', (request, response) => {
     // If the user doesn't enter input, loop
     twiml.redirect('/voice');
 
-    // Render the response as XML in reply to the webhook request
     response.type('text/xml');
     response.send(twiml.toString());
 });
 
 app.post('/gather', (request, response) => {
-    // Use the Twilio Node.js SDK to build an XML response
     const twiml = new VoiceResponse();
 
     // If the user entered digits, process their request
     if (request.body.Digits) {
-        console.log(request.body.CallSid);
-        console.log(request.body.Digits);
+        const env = request.body.Digits.charAt(5);
+        var API_BASE;
 
-        twiml.say('Leave your comment after the beep. Press any key or hang up when finished.');
+        switch (env) {
+            case 1:
+                API_BASE = process.env.API_BASE_LOCAL;
+                break;
+            case 2:
+                API_BASE = process.env.API_BASE_STAGING;
+                break;
+            default:
+                API_BASE = process.env.API_BASE;
+                break;
+        }
+
+        // attach CallSid to calling_code
+        console.log({
+            call_sid: request.body.CallSid,
+            calling_code: request.body.Digits.substring(0, 4),
+        })
+
+        // initiate voice recording
+        twiml.say('Leave your comment after the beep. When finished, press any key or hang up');
 
         twiml.record({
             action: '/record',
@@ -44,11 +61,9 @@ app.post('/gather', (request, response) => {
 
         twiml.hangup();
     } else {
-        // If no input was sent, redirect to the /voice route
         twiml.redirect('/voice');
     }
 
-    // Render the response as XML in reply to the webhook request
     response.type('text/xml');
     response.send(twiml.toString());
 });
@@ -56,10 +71,11 @@ app.post('/gather', (request, response) => {
 app.post('/record', (request, response) => {
     const twiml = new VoiceResponse();
 
-    console.log(request.body.CallSid);
-    console.log(request.body.RecordingUrl);
-
-    // TODO: Delete recording
+    // save recording
+    console.log({
+        call_sid: request.body.CallSid,
+        recording_url: request.body.RecordingUrl,
+    })
 
     response.type('text/xml');
     response.send(twiml.toString());
@@ -68,11 +84,12 @@ app.post('/record', (request, response) => {
 app.post('/transcribe', (request, response) => {
     const twiml = new VoiceResponse();
 
-    console.log(request.body.CallSid);
-    console.log(request.body.RecordingUrl);
-    console.log(request.body.TranscriptionText);
-
-    // TODO: Delete transcription?
+    // save transcript
+    console.log({
+        call_sid: request.body.CallSid,
+        recording_url: request.body.RecordingUrl,
+        transcription_text: request.body.TranscriptionText,
+    })
 
     response.type('text/xml');
     response.send(twiml.toString());
